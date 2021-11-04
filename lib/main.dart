7403ff31fe5dce20b3f0e107cc007343a76666bb
caishanglin <shang_cai@appdet.com>
2021-11-04 11:05:20 +0800

    2021/11/4 1.使用另一種bloc還是不太會用,到底Event該怎麼去切割,到底狀態該放在Event還是Filter裡面？

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_mask_map/Home/MainViewModel.dart';
-import 'package:flutter_mask_map/Home/MaskDataService.dart';
-import 'package:flutter_mask_map/Api/StateResponse.dart';
-import 'package:flutter_mask_map/Bean/mask_bean.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'Home/HomeAcitvity.dart';
 
